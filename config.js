3d8f1a0e41dab801ba71c0215037f29889cd0d76
ahmadtaim450-lang <ahmadtaim450@gmail.com>
2026-06-13 14:19:04 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -7,13 +7,13 @@
 /* (1) إعدادات مشروع Firebase
    من: Firebase Console → ⚙️ Project settings → Your apps → Web app → SDK config */
 window.FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyALjoxDqMB0W1Iq2ZOJvVviqg7ZNEnITF4",
-  authDomain:        "testdoc-abc01.firebaseapp.com",
-  projectId:         "testdoc-abc01",
-  storageBucket:     "testdoc-abc01.firebasestorage.app",
-  messagingSenderId: "790469564348",
-  appId:             "1:790469564348:web:0a5b58912300acc7b03db9",
-  measurementId:     "G-MDN0P4JYG7"
+   apiKey: "AIzaSyBOiLLRqSykhwzYrrFrdRO2mzWTYDU2W1M",
+  authDomain: "savedatatest-4dc4c.firebaseapp.com",
+  databaseURL: "https://savedatatest-4dc4c-default-rtdb.firebaseio.com",
+  projectId: "savedatatest-4dc4c",
+  storageBucket: "savedatatest-4dc4c.firebasestorage.app",
+  messagingSenderId: "64405748598",
+  appId: "1:64405748598:web:a0c2282d07d67c63a41857"
 };
 
 /* (2) حسابات جوجل للموظفين — نفس هذه الإيميلات يجب أن تُوضع في قواعد Firestore */
